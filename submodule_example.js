1b18730aae205f031562c0a17b168544eabbc616
wolf-dominion <hannahdruryutd@gmail.com>
2020-08-28 15:56:26 -0600

    incrememnt button

--- submodule_example.js
+++ submodule_example.js
@@ -5,3 +5,27 @@
 // I'm adding this line of code through the main project, "cd"ing into submodule_example and its branch "bugfix_01"
 
 // I am bugfix_02
+
+import React, { Component } from 'react'
+
+class submodule_example extends Component {
+
+    state = {
+        number: 0,
+    }
+
+    handleClick = () => {
+        this.setState({number: this.state.number + 1})
+    }
+
+    render(){
+        return(
+            <div>
+                <p>This text is within the submodule</p>
+                <p>Number: {this.state.number}</p>
+            </div>
+        )
+    }
+}
+
+export default submodule_example
